7748b6f1955e5c013d902128984070faaf3af956
Netanel-Iyov <nati16368447@gmail.com>
2023-11-20 09:04:13 +0000

    throwing exceptions when errors occurs and changing record_name to jenkins

--- misc/update_DNS_record.py
+++ misc/update_DNS_record.py
@@ -7,14 +7,14 @@ url = 'http://ipinfo.io'
 response = requests.get(url)
 new_ip = response.json()['ip']
 
-# Replace with your GoDaddy API key and secret
+# GoDaddy API key and secret
 api_key = os.environ['api_key']
 api_secret = os.environ['api_secret']
 
-# Replace with your GoDaddy domain and record details
+# GoDaddy details
 domain = 'niyov.com'
-record_name = '@'  # Replace with your record name
-record_type = 'A'  # Replace with your record type (A, CNAME, etc.)
+record_name = 'jenkins'  # record name
+record_type = 'A'  # record type
 
 # Authenticate with GoDaddy API
 url = f'https://api.godaddy.com/v1/domains/{domain}/records/{record_type}/{record_name}'
@@ -25,14 +25,21 @@ headers = {
 
 # Get current DNS record details
 response = requests.get(url, headers=headers)
+if not response.status_code in range(200, 400):
+    raise Exception(
+        f"Error: GoDaddy API GET request at {url} returned status code {response.status_code}")
+
+response_json = response.json()
+if not response_json:
+    raise Exception(f"response.hson returned {response_json}")
+
 data = response.json()[0]
 
 # Update the IP address in the DNS record
 current_ip = data.get('data', None)
 if not current_ip:
-    print(
-        f'Failed to fetch current DNS record details. Status code: {response.status_code}, Response: {response.text}')
-    sys.exit(1)
+    raise Exception(
+        f"Failed to fetch current DNS record details. Status code: {response.status_code}, Response: {response.text}")
 
 # Check if IP change is needed
 if current_ip == new_ip:
@@ -44,9 +51,8 @@ data['data'] = new_ip
 response = requests.put(url, json=[data], headers=headers)
 
 # Check if the update was successful
-if response.status_code == 200:
+if response.status_code in range(200, 400):
     print(f'DNS record updated successfully')
 else:
-    print(
-        f'Failed to update DNS record. Status code: {response.status_code}, Response: {response.text}')
-    sys.exit(1)
+    raise Exception(
+        f"Error: GoDaddy API PUT request at {url} returned status code {response.status_code}")
