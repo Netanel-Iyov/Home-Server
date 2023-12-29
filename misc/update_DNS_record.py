aea3645b31a924033dc7a82021b7c3159325776e
Netanel-Iyov <nati16368447@gmail.com>
2023-12-29 10:47:05 +0000

    making the script fetch each subdomain and act on that

--- misc/update_DNS_record.py
+++ misc/update_DNS_record.py
@@ -26,39 +26,41 @@ def update_dns_record(domain, record_names):
     api_secret = os.environ['api_secret']
 
     record_type = 'A'
-    # Authenticate with GoDaddy API
-    url = f'https://api.godaddy.com/v1/domains/{domain}/records/{record_type}/@'
-    headers = {
-        'Authorization': f'sso-key {api_key}:{api_secret}',
-        'Content-Type': 'application/json'
-    }
 
-    # Get current DNS record details
-    response = requests.get(url, headers=headers)
-    if not response.status_code in range(200, 400):
-        raise Exception(
-            f"Error: GoDaddy API GET request at {url} returned status code {response.status_code}")
-
-    response_json = response.json()
-    if not response_json:
-        raise Exception(f"response.json returned {response_json}")
-
-    data = response.json()[0]
-
-    # Update the IP address in the DNS record
-    current_ip = data.get('data', None)
-    if not current_ip:
-        raise Exception(
-            f"Failed to fetch current DNS record details. Status code: {response.status_code}, Response: {response.text}")
-
-    # Check if IP change is needed
-    if current_ip == new_ip:
-        print('IP was not changed')
-        sys.exit(0)
-
-    # Set new IP Address
-    data['data'] = new_ip
     for record_name in record_names:
+        # Authenticate with GoDaddy API
+        url = f'https://api.godaddy.com/v1/domains/{domain}/records/{record_type}/{record_name}'
+        headers = {
+            'Authorization': f'sso-key {api_key}:{api_secret}',
+            'Content-Type': 'application/json'
+        }
+
+        # Get current DNS record details
+        response = requests.get(url, headers=headers)
+        if not response.status_code in range(200, 400):
+            raise Exception(
+                f"Error: GoDaddy API GET request at {url} returned status code {response.status_code}")
+
+        response_json = response.json()
+        if not response_json:
+            raise Exception(f"response.json returned {response_json}")
+
+        data = response.json()[0]
+
+        # Update the IP address in the DNS record
+        current_ip = data.get('data', None)
+        if not current_ip:
+            raise Exception(
+                f"Failed to fetch current DNS record details. Status code: {response.status_code}, Response: {response.text}")
+
+        # Check if IP change is needed
+        if current_ip == new_ip:
+            print(f'IP was not changed for {record_name}')
+            continue
+
+        # Set new IP Address
+        data['data'] = new_ip
+
         data['name'] = record_name
         url = f'https://api.godaddy.com/v1/domains/{domain}/records/{record_type}/{record_name}'
         response = requests.put(url, json=[data], headers=headers)
